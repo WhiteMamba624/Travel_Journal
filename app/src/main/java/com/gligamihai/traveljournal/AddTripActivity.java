3500e49386d09a8dbb09e5e6f6a8cc0f9e073920
Mihai Gliga <mihaigliga99@yahoo.com>
2021-12-03 18:30:31 +0200

    Added Add Trip activity

---
+++ app/src/main/java/com/gligamihai/traveljournal/AddTripActivity.java
@@ -0,0 +1,84 @@
+package com.gligamihai.traveljournal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.SeekBar;
+
+import java.util.Calendar;
+
+public class AddTripActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final Calendar calendar = Calendar.getInstance();
+    private SeekBar priceSeekbar;
+    private EditText tripPrice;
+    private EditText startTripDate;
+    private EditText endTripDate;
+    private Button buttonSetStartTripDate;
+    private Button buttonSetEndTripDate;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+        priceSeekbar = findViewById(R.id.tripPriceSlider);
+        tripPrice = findViewById(R.id.tripPrice);
+        startTripDate = findViewById(R.id.startTripDate);
+        endTripDate = findViewById(R.id.endTripDate);
+        buttonSetStartTripDate=findViewById(R.id.buttonStartDate);
+        buttonSetEndTripDate=findViewById(R.id.buttonEndDate);
+        buttonSetEndTripDate.setOnClickListener(this);
+        buttonSetStartTripDate.setOnClickListener(this);
+        handlingSeekbar();
+    }
+
+    private void handlingSeekbar() {
+        priceSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tripPrice.setText("Price " + String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        String dateText=((Button)view).getText().toString();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        if (dateText.equalsIgnoreCase("Start date")) {
+                            startTripDate.setText(String.format("%d-%d-%d", dayOfMonth, monthOfYear + 1, year));
+                        } else
+                            if(dateText.equalsIgnoreCase("End date")){
+                            endTripDate.setText(String.format("%d-%d-%d", dayOfMonth, monthOfYear + 1, year));
+                        }
+                    }
+                }, year, month, day);
+        datePickerDialog.show();
+    }
+}
--- app/src/main/java/com/gligamihai/traveljournal/MainActivity.java
+++ app/src/main/java/com/gligamihai/traveljournal/MainActivity.java
@@ -1,20 +1,16 @@
 package com.gligamihai.traveljournal;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,12 +18,11 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.gligamihai.traveljournal.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+    // private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,31 +53,31 @@ public class MainActivity extends AppCompatActivity {
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
 //        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
 //        NavigationUI.setupWithNavController(navigationView, navController);
-  }
-        private void handlingNavigationBar() {
-            Toolbar toolbar = findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
+    }
 
-            DrawerLayout drawer = findViewById(R.id.drawer_layout);
-            NavigationView navigationView = findViewById(R.id.nav_view);
-            mAppBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.nav_home, R.id.nav_about_us, R.id.nav_contact_us,R.id.nav_share)
-                    .setOpenableLayout(drawer)
-                    .build();
-            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-            NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-            NavigationUI.setupWithNavController(navigationView, navController);
-        }
-        private void handlingFab(){
-            FloatingActionButton fab = findViewById(R.id.fab);
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                   Toast.makeText(MainActivity.this,"FAB is working",Toast.LENGTH_LONG).show();
-                }
-            });
+    private void handlingNavigationBar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_about_us, R.id.nav_contact_us, R.id.nav_share)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void handlingFab() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+//                   Toast.makeText(MainActivity.this,"FAB is working",Toast.LENGTH_LONG).show();
+            goToAddTripActivity();
+        });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,6 +93,10 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
+    private void goToAddTripActivity() {
+        Intent goToAddTripActivity = new Intent(MainActivity.this, AddTripActivity.class);
+        startActivity(goToAddTripActivity);
+    }
 
 
 }
